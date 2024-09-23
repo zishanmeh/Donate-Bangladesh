356569f3e4281f995b0de1bc6a542c54f585f6ad
zishanmeh <zishanmehedihasan2@gmail.com>
2024-09-23 02:22:35 +0100

    Added functionality for adding donation

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,75 @@
-console.log("HI");
+// Donate Noakhali
+
+document
+  .getElementById("donate-noakhali")
+  .addEventListener("click", function () {
+    const availableBalance = getAmountById("available_balance");
+    const amount = getInputAmount("amount-for-noakhali");
+    const total_noakhali_donation = getAmountById("total_noakhali_donation");
+    // Validation
+    if (amount > availableBalance || amount <= 0 || isNaN(amount) === true) {
+      alert("Invalid input! Please check!!!");
+      return;
+    }
+
+    // Adding donate
+    addDonate(
+      amount,
+      total_noakhali_donation,
+      availableBalance,
+      "total_noakhali_donation"
+    );
+    document.getElementById("amount-for-noakhali").value = "";
+    // Adding in history
+    const newTime = new Date();
+    const heading = document.getElementById("noakhali_heading").innerText;
+    addDonationHistory(newTime, amount, heading);
+  });
+
+//   Donate feni
+
+document
+  .getElementById("donate-feni-btn")
+  .addEventListener("click", function () {
+    const availableBalance = getAmountById("available_balance");
+    const amount = getInputAmount("amount-for-feni");
+    const total_donation = getAmountById("total_feni_donation");
+    // Validation
+    if (amount > availableBalance || amount <= 0 || isNaN(amount) === true) {
+      alert("Invalid input! Please check!!!");
+      return;
+    }
+    //   Adding donate
+    addDonate(amount, total_donation, availableBalance, "total_feni_donation");
+    document.getElementById("amount-for-feni").value = "";
+    // Adding in history
+    const newTime = new Date();
+    const heading = document.getElementById("feni_heading").innerText;
+    addDonationHistory(newTime, amount, heading);
+  });
+
+//   Donate for injured
+document.getElementById("quota-btn").addEventListener("click", function () {
+  const availableBalance = getAmountById("available_balance");
+  const amount = getInputAmount("amount-for-injured");
+  const total_donation = getAmountById("total_donation_for_injured");
+  //   Validation
+  // Validation
+  if (amount > availableBalance || amount <= 0 || isNaN(amount) === true) {
+    alert("Invalid input! Please check!!!");
+    return;
+  }
+  //   Adding donate
+  addDonate(
+    amount,
+    total_donation,
+    availableBalance,
+    "total_donation_for_injured"
+  );
+  document.getElementById("amount-for-injured").value = "";
+
+  // Adding in history
+  const newTime = new Date();
+  const heading = document.getElementById("quota_heading").innerText;
+  addDonationHistory(newTime, amount, heading);
+});
--- src/js/utilites.js
+++ src/js/utilites.js
@@ -1,1 +1,39 @@
-console.log("HEllo");
+// Convert string to Number and get Value
+function getAmountById(id) {
+  const value = document.getElementById(id).innerText;
+  const valueInFloat = parseFloat(value);
+  return valueInFloat;
+}
+
+// getting input amount
+function getInputAmount(id) {
+  const inputValue = document.getElementById(id).value;
+  const float_inputValue = parseFloat(inputValue);
+  return float_inputValue;
+}
+
+// Function for Adding donate;
+function addDonate(
+  input_amount,
+  total_donationed_amount,
+  available_balance,
+  id_for_toatal_donated_amount
+) {
+  total_donationed_amount += input_amount;
+  available_balance -= input_amount;
+  const show_total = document.getElementById(id_for_toatal_donated_amount);
+  show_total.innerText = total_donationed_amount;
+  document.getElementById("available_balance").innerText = available_balance;
+}
+
+// Add donation history
+
+function addDonationHistory(time, donate_amount, heading) {
+  const historyPage = document.getElementById("history-page");
+  historyPage.innerHTML += `
+    <div class="border border-dark3 p-3 rounded-lg shadow-md w-full mb-4">
+        <h1 class="text-2xl font-bold text-dark mb-3">${donate_amount} Taka has been ${heading}</h1>
+        <p>Date: ${time}</p>
+    </div>
+    `;
+}
