dd07618ec41284f27fd694a9fc976e7addf9fd30
zishanmeh <zishanmehedihasan2@gmail.com>
2024-09-23 12:13:22 +0100

    Simple changes to avoid bug

--- src/js/utilites.js
+++ src/js/utilites.js
@@ -22,8 +22,9 @@ function addDonate(
   total_donationed_amount += input_amount;
   available_balance -= input_amount;
   const show_total = document.getElementById(id_for_toatal_donated_amount);
-  show_total.innerText = total_donationed_amount;
-  document.getElementById("available_balance").innerText = available_balance;
+  show_total.innerText = total_donationed_amount.toFixed(2);
+  document.getElementById("available_balance").innerText =
+    available_balance.toFixed(2);
 }
 
 // Add donation history
@@ -35,7 +36,9 @@ function addDonationHistory(time, donate_amount, heading) {
   }
   historyPage.innerHTML += `
     <div class="border border-dark3 p-3 rounded-lg shadow-md w-full mb-4">
-        <h1 class="text-2xl font-bold text-dark mb-3">${donate_amount} Taka has been ${heading}</h1>
+        <h1 class="text-2xl font-bold text-dark mb-3">${donate_amount.toFixed(
+          2
+        )} Taka has been ${heading}</h1>
         <p>Date: ${time}</p>
     </div>
     `;
